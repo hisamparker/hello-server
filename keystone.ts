6efab6e7d74139c85013b6db9bdd6f39a837e9b6
sam parker <hisamparker@gmail.com>
2021-08-28 14:44:06 +0200

    *

--- keystone.ts
+++ keystone.ts
@@ -57,7 +57,7 @@ export default withAuth(
     server: {
       // set cors to recognize our local front end server during development
       cors: {
-        origin: 'https://upbeat-wozniak-ff6ab9.netlify.app/',
+        origin: '*',
         credentials: true,
       },
     },
