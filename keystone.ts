1ca10794286220a83e83177814e4b48101e13e13
sam parker <hisamparker@gmail.com>
2021-08-30 10:46:00 +0200

    set path

--- keystone.ts
+++ keystone.ts
@@ -22,6 +22,7 @@ const databaseURL = process.env.DATABASE_URL;
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 365, // how long user stays signed in
   secret: process.env.COOKIE_SECRET,
+  path: '/',
   sameSite: 'None',
   secure: true,
 };
